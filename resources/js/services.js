ee3d427951f31fcd64ad8baab2cd03ba27797b73
Waleed Faisal <waleedfaisal94@gmail.com>
2021-07-04 20:38:33 +0500

    content-changes

--- resources/js/services.js
+++ resources/js/services.js
@@ -23,6 +23,24 @@ $('#Image03Div').mouseleave(function() {
     $('#Image03').next().addClass("closed");
 });
 
+$('#Image04Div').hover(function() {
+    $('#Image04').next().removeClass("closed");
+    $('#Image04').next().addClass("opened");
+});
+$('#Image04Div').mouseleave(function() {
+    $('#Image04').next().removeClass("opened");
+    $('#Image04').next().addClass("closed");
+});
+
+$('#Image05Div').hover(function() {
+    $('#Image05').next().removeClass("closed");
+    $('#Image05').next().addClass("opened");
+});
+$('#Image05Div').mouseleave(function() {
+    $('#Image05').next().removeClass("opened");
+    $('#Image05').next().addClass("closed");
+});
+
 setTimeout(function() {
     console.log('Hello world')
     $('#span1').css('visibility', 'visible');
